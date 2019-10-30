e11d81d008047e1e0e8ce48d270169436f5d29cd
Jared Noble <jsnoble1@gmail.com>
2019-10-30 09:51:51 -0700

    state storage migration

---
+++ asset/src/elasticsearch_state_storage/api.ts
@@ -0,0 +1,39 @@
+
+import { OperationAPI, WorkerContext, ExecutionConfig } from '@terascope/job-components';
+import { ESCachedStateStorage } from '@terascope/teraslice-state-storage';
+import { ESStateStorageConfig } from './interfaces';
+
+class ElasticsearchStateStorage extends OperationAPI {
+    stateStorage: ESCachedStateStorage;
+
+    constructor(
+        context: WorkerContext,
+        apiConfig: ESStateStorageConfig,
+        executionConfig: ExecutionConfig
+    ) {
+        super(context, apiConfig, executionConfig);
+        const { client } = this.context.foundation.getConnection({
+            endpoint: this.apiConfig.connection,
+            type: 'elasticsearch',
+            cached: true
+        });
+
+        this.stateStorage = new ESCachedStateStorage(client, this.logger, this.apiConfig);
+    }
+
+    async initialize() {
+        await super.initialize();
+        await this.stateStorage.initialize();
+    }
+
+    async shutdown() {
+        await super.shutdown();
+        await this.stateStorage.shutdown();
+    }
+
+    async createAPI() {
+        return this.stateStorage;
+    }
+}
+
+module.exports = ElasticsearchStateStorage;
