49e7a009a6eb77f06e184d1b216e5dd14f581994
Jared Noble <jsnoble1@gmail.com>
2020-01-23 12:13:55 -0700

    data generator is now recoverable, update packages

--- test/elasticsearch_data_generator-spec.ts
+++ test/elasticsearch_data_generator-spec.ts
@@ -1,9 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import { SlicerRecoveryData } from '@terascope/job-components';
+import { SlicerRecoveryData, LifeCycle } from '@terascope/job-components';
 import { WorkerTestHarness, SlicerTestHarness, newTestJobConfig } from 'teraslice-test-harness';
 import path from 'path';
 import MockClient from './mock_client';
+
+interface SlicerTestArgs {
+    opConfig: any;
+    numOfSlicers?: number;
+    recoveryData?: SlicerRecoveryData[];
+    lifecycle?: LifeCycle;
+    size: number;
+}
 
 describe('elasticsearch_data_generator', () => {
     const assetDir = path.join(__dirname, '..');
@@ -34,22 +42,24 @@ describe('elasticsearch_data_generator', () => {
         return harness;
     }
 
-    async function makeSlicerTest(
-        config: any,
+    async function makeSlicerTest({
+        opConfig,
         numOfSlicers = 1,
-        recoveryData?: SlicerRecoveryData[],
-        size?: number
-    ) {
+        recoveryData,
+        lifecycle = 'once',
+        size
+    }: SlicerTestArgs) {
         const job = newTestJobConfig({
             analytics: true,
             slicers: numOfSlicers,
+            lifecycle,
             operations: [
-                config,
+                opConfig,
                 {
                     _op: 'noop',
                     size
                 }
-            ]
+            ],
         });
         harness = new SlicerTestHarness(job, { assetDir, clients });
         await harness.initialize(recoveryData);
@@ -82,27 +92,80 @@ describe('elasticsearch_data_generator', () => {
     describe('slicer', () => {
         it('in "once" mode will return number based off total size of last op', async () => {
             const opConfig = { _op: 'elasticsearch_data_generator', size: 15 };
-            const test = await makeSlicerTest(opConfig, 1, [], 5);
+            const config: SlicerTestArgs = {
+                opConfig,
+                numOfSlicers: 1,
+                size: 5
+            };
+
+            const test = await makeSlicerTest(config);
 
             const [slice1] = await test.createSlices();
-            expect(slice1).toEqual({ count: 5 });
+            expect(slice1).toEqual({ count: 5, processed: 5 });
 
             const [slice2] = await test.createSlices();
-            expect(slice2).toEqual({ count: 5 });
+            expect(slice2).toEqual({ count: 5, processed: 10 });
 
             const [slice3] = await test.createSlices();
-            expect(slice3).toEqual({ count: 5 });
+            expect(slice3).toEqual({ count: 5, processed: 15 });
 
             const [slice4] = await test.createSlices();
             expect(slice4).toEqual(null);
         });
 
-        it('in "once" mode will return number based off total size, which can consume it all', async () => {
-            const opConfig = { _op: 'elasticsearch_data_generator', size: 15 };
-            const test = await makeSlicerTest(opConfig, 1, [], 5000);
+        it('in "once" mode can deal with uneven slice numbers', async () => {
+            const opConfig = { _op: 'elasticsearch_data_generator', size: 12 };
+            const config: SlicerTestArgs = {
+                opConfig,
+                numOfSlicers: 1,
+                size: 5
+            };
+            const test = await makeSlicerTest(config);
 
             const [slice1] = await test.createSlices();
-            expect(slice1).toEqual({ count: 15 });
+            expect(slice1).toEqual({ count: 5, processed: 5 });
+
+            const [slice2] = await test.createSlices();
+            expect(slice2).toEqual({ count: 5, processed: 10 });
+
+            const [slice3] = await test.createSlices();
+            expect(slice3).toEqual({ count: 2, processed: 12 });
+
+            const [slice4] = await test.createSlices();
+            expect(slice4).toEqual(null);
+        });
+
+        it('can run recovery in once mode', async () => {
+            const opConfig = { _op: 'elasticsearch_data_generator', size: 15 };
+            const config: SlicerTestArgs = {
+                opConfig,
+                numOfSlicers: 1,
+                recoveryData: [{ lastSlice: { count: 5, processed: 5 }, slicer_id: 0 }],
+                size: 5
+            };
+            const test = await makeSlicerTest(config);
+
+            const [slice1] = await test.createSlices();
+            expect(slice1).toEqual({ count: 5, processed: 10 });
+
+            const [slice2] = await test.createSlices();
+            expect(slice2).toEqual({ count: 5, processed: 15 });
+
+            const [slice3] = await test.createSlices();
+            expect(slice3).toEqual(null);
+        });
+
+        it('in "once" mode will return number based off total size, which can consume it all', async () => {
+            const opConfig = { _op: 'elasticsearch_data_generator', size: 15 };
+            const config: SlicerTestArgs = {
+                opConfig,
+                numOfSlicers: 1,
+                size: 5000
+            };
+            const test = await makeSlicerTest(config);
+
+            const [slice1] = await test.createSlices();
+            expect(slice1).toEqual({ count: 15, processed: 15 });
 
             const [slice2] = await test.createSlices();
             expect(slice2).toEqual(null);
@@ -116,52 +179,35 @@ describe('elasticsearch_data_generator', () => {
 
         it('slicer in "persistent" mode will continuously produce the same number', async () => {
             const opConfig = { _op: 'elasticsearch_data_generator', size: 550 };
-            const job = newTestJobConfig({
-                analytics: true,
+            const config: SlicerTestArgs = {
                 lifecycle: 'persistent',
-                slicers: 1,
-                operations: [
-                    opConfig,
-                    {
-                        _op: 'noop',
-                        size: 5000
-                    }
-                ]
-            });
-            const testHarness = new SlicerTestHarness(job, { assetDir, clients });
-            await testHarness.initialize();
+                numOfSlicers: 1,
+                opConfig,
+                size: 5000
+            };
+            const testHarness = await makeSlicerTest(config);
 
             const results1 = await testHarness.createSlices();
             const results2 = await testHarness.createSlices();
             const results3 = await testHarness.createSlices();
 
-            expect(results1).toEqual([{ count: 550 }]);
-            expect(results2).toEqual([{ count: 550 }]);
-            expect(results3).toEqual([{ count: 550 }]);
-
-            await testHarness.shutdown();
+            expect(results1).toEqual([{ count: 5000, processed: 5000 }]);
+            expect(results2).toEqual([{ count: 5000, processed: 10000 }]);
+            expect(results3).toEqual([{ count: 5000, processed: 15000 }]);
         });
 
         it('data generator will only return one slicer', async () => {
             const opConfig = { _op: 'elasticsearch_data_generator', size: 550 };
-            const job = newTestJobConfig({
-                analytics: true,
+            const config: SlicerTestArgs = {
                 lifecycle: 'persistent',
-                slicers: 3,
-                operations: [
-                    opConfig,
-                    {
-                        _op: 'noop',
-                        size: 5000
-                    }
-                ]
-            });
-            const testHarness = new SlicerTestHarness(job, { assetDir, clients });
-            await testHarness.initialize();
+                numOfSlicers: 3,
+                opConfig,
+                size: 5000
+            };
+            const testHarness = await makeSlicerTest(config);
             const slicer = testHarness.slicer();
 
             expect(slicer.slicers()).toEqual(1);
-            await testHarness.shutdown();
         });
     });
 });
--- test/elasticsearch_date/date_slicer_fn-spec.ts
+++ test/elasticsearch_date/date_slicer_fn-spec.ts
@@ -1,5 +1,5 @@
 import 'jest-extended';
-import EventEmitter from 'events';
+import { EventEmitter } from 'events';
 import {
     newTestExecutionConfig,
     LifeCycle,
