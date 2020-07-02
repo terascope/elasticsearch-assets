0e93788bf903ad80f85d882d7de566f0bf4a386d
Jared Noble <jsnoble1@gmail.com>
2020-07-02 13:48:44 -0700

    added 5.6 client api version

--- test/helpers/config.ts
+++ test/helpers/config.ts
@@ -2,5 +2,6 @@ const { TEST_INDEX_PREFIX = 'es_assets_', ELASTICSEARCH_HOST = 'http://localhost
 let version = process.env.ELASTICSEARCH_VERSION;
 if (!version || version.charAt(0) === '6') version = '6.5';
 if (version.charAt(0) === '7') version = '7.x';
+if (version.charAt(0) === '5') version = '5.6';
 const ELASTICSEARCH_VERSION = version;
 export { TEST_INDEX_PREFIX, ELASTICSEARCH_HOST, ELASTICSEARCH_VERSION };
