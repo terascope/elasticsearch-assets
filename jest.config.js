c28635dd5927a5d38eefa23bdb50bcf6918a6f03
peterdemartini <thepeterdemartini@gmail.com>
2021-03-03 12:47:10 -0700

    Fix tests to use the source files instead of the compiled files

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const path = require('path');
+
 module.exports = {
     verbose: true,
     testEnvironment: 'node',
@@ -20,6 +22,10 @@ module.exports = {
         '<rootDir>/test/**/*-spec.{ts,js}',
         '<rootDir>/test/*-spec.{ts,js}',
     ],
+    moduleNameMapper: {
+        '^@terascope/elasticsearch-asset-apis$': path.join(__dirname, '/packages/elasticsearch-asset-apis/src/index.ts'),
+        '^terafoundation_elasticsearch_connector$': path.join(__dirname, '/packages/terafoundation_elasticsearch_connector/src/index.ts'),
+    },
     preset: 'ts-jest',
     globals: {
         'ts-jest': {
