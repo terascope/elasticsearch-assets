e1226c28dcdaa31868699122dcb8945bded14139
peterdemartini <thepeterdemartini@gmail.com>
2021-07-15 12:42:12 -0700

    Add better spaces request error handling

---
+++ packages/elasticsearch-asset-apis/src/elasticsearch-reader-api/throwRequestError.ts
@@ -0,0 +1,45 @@
+import {
+    has, tryParseJSON, TSError
+} from '@terascope/utils';
+import { inspect } from 'util';
+
+/**
+ * Try and proxy the error as best as possible to avoid confusing errors
+*/
+export function throwRequestError(endpointName: string, statusCode: number, body: unknown): never {
+    const resp = tryParseJSON(body);
+
+    if (resp && has(resp, 'error')) {
+        if (resp.debug) {
+            const err = new Error(resp.debug.message);
+            if (resp.debug.stack) {
+                err.stack = resp.debug.stack;
+            }
+            if (resp.debug.statusCode) {
+                // @ts-expect-error
+                err.statusCode = resp.debug.statusCode;
+            }
+            throw new TSError(err, {
+                defaultStatusCode: statusCode,
+                reason: resp.error,
+                context: {
+                    endpoint: endpointName,
+                    safe: false
+                }
+            });
+        } else {
+            throw new TSError(resp.error, {
+                statusCode,
+                context: {
+                    endpoint: endpointName,
+                    safe: true
+                }
+            });
+        }
+    }
+
+    // fallback error handling
+    throw new TSError(`Unknown Request Error: ${inspect(resp, { depth: 10 })}`, {
+        statusCode
+    });
+}
