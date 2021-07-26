ab797f09dc0033c9535a01ca9f544b96a0903b09
peterdemartini <thepeterdemartini@gmail.com>
2021-07-26 08:37:54 -0700

    Fix id slice tests

--- test/id_reader/slicer-spec.ts
+++ test/id_reader/slicer-spec.ts
@@ -8,7 +8,6 @@ import {
     makeClient,
     populateIndex,
     waitForData,
-    formatWildcardQuery
 } from '../helpers';
 import evenSpread from '../fixtures/data/even-spread';
 
@@ -107,24 +106,24 @@ describe('id_reader slicer', () => {
         // null shows we are done, we remove to compare objects
         expect(results.pop()).toBeNull();
 
-        const expectedResults = formatWildcardQuery([
-            { key: 'a*', count: 58 },
-            { key: 'b*', count: 82 },
-            { key: 'c*', count: 64 },
-            { key: 'd*', count: 49 },
-            { key: 'e*', count: 59 },
-            { key: 'f*', count: 51 },
-            { key: '0*', count: 70 },
-            { key: '1*', count: 55 },
-            { key: '2*', count: 55 },
-            { key: '3*', count: 54 },
-            { key: '4*', count: 68 },
-            { key: '5*', count: 64 },
-            { key: '6*', count: 52 },
-            { key: '7*', count: 80 },
-            { key: '8*', count: 75 },
-            { key: '9*', count: 64 },
-        ], version, docType, field);
+        const expectedResults = [
+            { keys: ['a'], count: 58 },
+            { keys: ['b'], count: 82 },
+            { keys: ['c'], count: 64 },
+            { keys: ['d'], count: 49 },
+            { keys: ['e'], count: 59 },
+            { keys: ['f'], count: 51 },
+            { keys: ['0'], count: 70 },
+            { keys: ['1'], count: 55 },
+            { keys: ['2'], count: 55 },
+            { keys: ['3'], count: 54 },
+            { keys: ['4'], count: 68 },
+            { keys: ['5'], count: 64 },
+            { keys: ['6'], count: 52 },
+            { keys: ['7'], count: 80 },
+            { keys: ['8'], count: 75 },
+            { keys: ['9'], count: 64 },
+        ];
 
         results.forEach((result, index) => {
             expect(result).toMatchObject(expectedResults[index]);
@@ -138,10 +137,10 @@ describe('id_reader slicer', () => {
         // null shows we are done, we remove to compare objects
         expect(results.pop()).toBeNull();
 
-        const expectedResults = formatWildcardQuery([
-            { key: 'a*', count: 58 },
-            { key: 'b*', count: 82 },
-        ], version, docType, field);
+        const expectedResults = [
+            { keys: ['a'], count: 58 },
+            { keys: ['b'], count: 82 },
+        ];
 
         results.forEach((result, index) => {
             expect(result).toMatchObject(expectedResults[index]);
@@ -163,23 +162,23 @@ describe('id_reader slicer', () => {
 
         expect(results.pop()).toBeNull();
 
-        const expectedResults = formatWildcardQuery([
-            { key: 'a0*', count: 5 },
-            { key: 'a1*', count: 7 },
-            { key: 'a3*', count: 2 },
-            { key: 'a4*', count: 3 },
-            { key: 'a5*', count: 3 },
-            { key: 'a6*', count: 3 },
-            { key: 'a7*', count: 4 },
-            { key: 'a8*', count: 5 },
-            { key: 'a9*', count: 8 },
-            { key: 'aa*', count: 3 },
-            { key: 'ab*', count: 3 },
-            { key: 'ac*', count: 3 },
-            { key: 'ad*', count: 4 },
-            { key: 'ae*', count: 4 },
-            { key: 'af*', count: 1 },
-        ], version, docType, field);
+        const expectedResults = [
+            { keys: ['a0'], count: 5 },
+            { keys: ['a1'], count: 7 },
+            { keys: ['a3'], count: 2 },
+            { keys: ['a4'], count: 3 },
+            { keys: ['a5'], count: 3 },
+            { keys: ['a6'], count: 3 },
+            { keys: ['a7'], count: 4 },
+            { keys: ['a8'], count: 5 },
+            { keys: ['a9'], count: 8 },
+            { keys: ['aa'], count: 3 },
+            { keys: ['ab'], count: 3 },
+            { keys: ['ac'], count: 3 },
+            { keys: ['ad'], count: 4 },
+            { keys: ['ae'], count: 4 },
+            { keys: ['af'], count: 1 },
+        ];
 
         results.forEach((result, index) => {
             expect(result).toMatchObject(expectedResults[index]);
@@ -195,57 +194,57 @@ describe('id_reader slicer', () => {
         const test = await makeSlicerTest(opConfig);
         const results = await test.getAllSlices();
 
-        const expectedResults = formatWildcardQuery([
-            { key: 'aa5*', count: 1 },
-            { key: 'aa6*', count: 1 },
-            { key: 'aa7*', count: 1 },
-            { key: 'aba*', count: 1 },
-            { key: 'abc*', count: 1 },
-            { key: 'ab4*', count: 1 },
-            { key: 'ac3*', count: 1 },
-            { key: 'ac5*', count: 1 },
-            { key: 'ac8*', count: 1 },
-            { key: 'ad1*', count: 1 },
-            { key: 'ad4*', count: 1 },
-            { key: 'ad5*', count: 1 },
-            { key: 'ad6*', count: 1 },
-            { key: 'aea*', count: 1 },
-            { key: 'aef*', count: 1 },
-            { key: 'ae0*', count: 1 },
-            { key: 'ae8*', count: 1 },
-            { key: 'af4*', count: 1 },
-            { key: 'a0c*', count: 1 },
-            { key: 'a0f*', count: 2 },
-            { key: 'a01*', count: 1 },
-            { key: 'a04*', count: 1 },
-            { key: 'a1b*', count: 1 },
-            { key: 'a1c*', count: 1 },
-            { key: 'a1e*', count: 1 },
-            { key: 'a1f*', count: 1 },
-            { key: 'a11*', count: 2 },
-            { key: 'a17*', count: 1 },
-            { key: 'a36*', count: 2 },
-            { key: 'a4e*', count: 1 },
-            { key: 'a42*', count: 1 },
-            { key: 'a43*', count: 1 },
-            { key: 'a50*', count: 1 },
-            { key: 'a52*', count: 1 },
-            { key: 'a53*', count: 1 },
-            { key: 'a6d*', count: 1 },
-            { key: 'a6e*', count: 1 },
-            { key: 'a69*', count: 1 },
-            { key: 'a7a*', count: 1 },
-            { key: 'a7d*', count: 2 },
-            { key: 'a7e*', count: 1 },
-            { key: 'a8d*', count: 1 },
-            { key: 'a8f*', count: 2 },
-            { key: 'a89*', count: 2 },
-            { key: 'a9d*', count: 1 },
-            { key: 'a9e*', count: 2 },
-            { key: 'a94*', count: 2 },
-            { key: 'a95*', count: 1 },
-            { key: 'a97*', count: 2 },
-        ], version, docType, field);
+        const expectedResults = [
+            { keys: ['aa5'], count: 1 },
+            { keys: ['aa6'], count: 1 },
+            { keys: ['aa7'], count: 1 },
+            { keys: ['aba'], count: 1 },
+            { keys: ['abc'], count: 1 },
+            { keys: ['ab4'], count: 1 },
+            { keys: ['ac3'], count: 1 },
+            { keys: ['ac5'], count: 1 },
+            { keys: ['ac8'], count: 1 },
+            { keys: ['ad1'], count: 1 },
+            { keys: ['ad4'], count: 1 },
+            { keys: ['ad5'], count: 1 },
+            { keys: ['ad6'], count: 1 },
+            { keys: ['aea'], count: 1 },
+            { keys: ['aef'], count: 1 },
+            { keys: ['ae0'], count: 1 },
+            { keys: ['ae8'], count: 1 },
+            { keys: ['af4'], count: 1 },
+            { keys: ['a0c'], count: 1 },
+            { keys: ['a0f'], count: 2 },
+            { keys: ['a01'], count: 1 },
+            { keys: ['a04'], count: 1 },
+            { keys: ['a1b'], count: 1 },
+            { keys: ['a1c'], count: 1 },
+            { keys: ['a1e'], count: 1 },
+            { keys: ['a1f'], count: 1 },
+            { keys: ['a11'], count: 2 },
+            { keys: ['a17'], count: 1 },
+            { keys: ['a36'], count: 2 },
+            { keys: ['a4e'], count: 1 },
+            { keys: ['a42'], count: 1 },
+            { keys: ['a43'], count: 1 },
+            { keys: ['a50'], count: 1 },
+            { keys: ['a52'], count: 1 },
+            { keys: ['a53'], count: 1 },
+            { keys: ['a6d'], count: 1 },
+            { keys: ['a6e'], count: 1 },
+            { keys: ['a69'], count: 1 },
+            { keys: ['a7a'], count: 1 },
+            { keys: ['a7d'], count: 2 },
+            { keys: ['a7e'], count: 1 },
+            { keys: ['a8d'], count: 1 },
+            { keys: ['a8f'], count: 2 },
+            { keys: ['a89'], count: 2 },
+            { keys: ['a9d'], count: 1 },
+            { keys: ['a9e'], count: 2 },
+            { keys: ['a94'], count: 2 },
+            { keys: ['a95'], count: 1 },
+            { keys: ['a97'], count: 2 },
+        ];
 
         expect(results.pop()).toBeNull();
 
@@ -279,9 +278,9 @@ describe('id_reader slicer', () => {
         if (!slices1) throw new Error('slice1 was not found');
         if (!slices2) throw new Error('slice2 was not found');
 
-        const expectedResults = formatWildcardQuery([
-            { key: 'a*', count: 58 }, { key: 'b*', count: 82 }
-        ], version, docType, field);
+        const expectedResults = [
+            { keys: ['a'], count: 58 }, { keys: ['b'], count: 82 }
+        ];
 
         expect(slices1.slicer_id).toEqual(0);
         expect(slices1.slicer_order).toEqual(1);
@@ -298,7 +297,7 @@ describe('id_reader slicer', () => {
     });
 
     it('can return to previous position', async () => {
-        const lastSlice = formatWildcardQuery([{ key: 'a6*', count: 3 }], version, docType, field)[0];
+        const lastSlice = { keys: ['a6'], count: 3 };
         const retryData = [{ lastSlice, slicer_id: 0 }];
         const opConfig = { key_range: ['a'] };
 
@@ -307,11 +306,11 @@ describe('id_reader slicer', () => {
 
         expect(results.pop()).toBeNull();
 
-        const expectedResults = formatWildcardQuery([
-            { key: 'a7*', count: 4 },
-            { key: 'a8*', count: 5 },
-            { key: 'a9*', count: 8 },
-        ], version, docType, field);
+        const expectedResults = [
+            { keys: ['a7'], count: 4 },
+            { keys: ['a8'], count: 5 },
+            { keys: ['a9'], count: 8 },
+        ];
 
         results.forEach((result, index) => {
             expect(result).toMatchObject(expectedResults[index]);
